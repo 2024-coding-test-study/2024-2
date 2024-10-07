25936ba4e1bb6351257c2ad016b45086d9246f66
Gjuni <kmj06178@gmail.com>
2024-10-07 15:20:17 +0900

    week 03 보강 하지만 틀립ㅁ

--- week03/최소 스패닝 트리/jun.java
+++ week03/최소 스패닝 트리/jun.java
@@ -3,21 +3,32 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 
-class Main {
-    static void add_graph(int[][] graph, int x, int y, int weight) {
-        graph[x][y] = weight;
-        graph[y][x] = weight;
+class jun {
+    /** LinkedArray 추가하는 함수 */
+    public static void add_graph(int x, int y, ArrayList<ArrayList<Integer>> graph, int weight) {
+        graph.get(x).add(y);
+        graph.get(x).add(weight);
+        graph.get(y).add(x);
+        graph.get(y).add(weight);
     }
 
-    static long DFS(int[][] graph, int start, long total) {
-        for(int i = 1; i< (graph[0].length); i++) {
-                total += graph[start][i]; // weight 추가
+    /** 각각의 idx 탐색 함수 */
+    public static long sum_of_graph(int start, ArrayList<ArrayList<Integer>> graph) {
+        long value = 0;
+
+        for(int i = 0; i< graph.get(start).size(); i+= 2) {
+            int des = graph.get(start).get(i);
+            int weight = graph.get(start).get(i+1);
+            value += weight;
         }
-        return total;
+
+        return value;
     }
+
 
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -27,8 +38,14 @@ class Main {
         int vertax = Integer.parseInt(token.nextToken()); // vertax
         int edge = Integer.parseInt(token.nextToken()); // edge
 
-        int[][] graph = new int[vertax+1][vertax+1]; // graph 값에 가중치를 넣으면 됨
+        /** Linked Array 추가 */
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
         
+        /** 각 정점에 대한 ArrayList 초기화 */
+        for(int i = 0; i<= vertax; i++) {
+            graph.add(new ArrayList<>());
+        }
+
         /** 깊이 우선 탐색으로 graph가 0이라면 더해주고 방문처리 */
 
         for(int i =1 ; i<= edge; i++) { // index = graph
@@ -37,24 +54,21 @@ class Main {
             int x = Integer.parseInt(token2.nextToken());
             int y = Integer.parseInt(token2.nextToken());
             int weight = Integer.parseInt(token2.nextToken());
-
-            add_graph(graph, x, y, weight);
+            
+            add_graph(x, y, graph, weight); // 그래프 추가
         }
 
-        long[] result_arr = new long[vertax+1];
-        long answer = 2147483647;
+        long[] result = new long[vertax+1];
+        long compare = 2147483647;
+                       
 
-        for(int i =1; i <= vertax; i++) {
-            long total = 0;
-            
-            result_arr[i] = DFS(graph, i, total);
-
-            if(result_arr[i] <= answer) {
-                answer = result_arr[i];
+        for(int i =1; i<= vertax; i++) {
+            result[i] = sum_of_graph(i, graph);
+            if (result[i] <= compare) {
+                compare = result[i];
             }
         }
-
-        output.write(String.valueOf(answer));
+        output.write(String.valueOf(compare));
 
         input.close();
         output.close();
