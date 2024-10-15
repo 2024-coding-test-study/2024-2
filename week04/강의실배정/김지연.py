4e746ac978af8265093272f9a4f035f55d9dc2c3
xxyeon <jykim9335@gmail.com>
2024-10-15 14:04:11 +0900

    fix: 강의실 배정 반례 해결했지만 시간초과

--- week04/강의실배정/김지연.py
+++ week04/강의실배정/김지연.py
@@ -1,22 +1,14 @@
 import heapq
 n = int(input())
 
-'''
-4
-1 2
-1 4
-2 6
-4 5
-'''
 time = []
 for _ in range(n):
     start, end = map(int, input().split())
     time.append((end, start))
-time.sort()
+time.sort(key=lambda x : x[1]) #시작 시간으로 정렬하는 이유: 이전 강의가 끝나는 시간에 맞춰서 다른 강의를 할당해 줄때 시작 시간이 빠른 순으로 할당해 줘야 최소의 강의실을 사용할 수 있음
 q = []
 heapq.heappush(q, time[0])
 for i in range(1, n):
-    print(q)
     prev_end, prev_start = heapq.heappop(q)
     end, start = time[i]
     if prev_end <= start: #이전 수업에 사용하던 강의실 배정
