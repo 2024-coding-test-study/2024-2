c0c8090f84917011082aaa7835d2799e64e0be2e
xxyeon <jykim9335@gmail.com>
2024-10-15 14:19:51 +0900

    refactor: time배열도 우선순위큐 사용해서 풀이
    혹시나 해서 time배열을 우선순위 큐로 바꿔서 풀이 했지만 시간초과는 해결 안되고 입력 받는 부분이 시간초과의 원인이였음을 알 수 있음

--- week04/강의실배정/김지연.py
+++ week04/강의실배정/김지연.py
@@ -5,19 +5,16 @@ n = int(sys.stdin.readline().rstrip())
 time = []
 
 for _ in range(n):
-    start, end = map(int, sys.stdin.readline().rstrip().split())#input()으로 입력받을 시 시간초과
-    time.append((end, start))
-    # heapq.heappush(time, (start, end))
-time.sort(key=lambda x : x[1]) #시작 시간으로 정렬하는 이유: 이전 강의가 끝나는 시간에 맞춰서 다른 강의를 할당해 줄때 시작 시간이 빠른 순으로 할당해 줘야 최소의 강의실을 사용할 수 있음
+    start, end = map(int, sys.stdin.readline().rstrip().split())
+    # time.append((end, start))
+    heapq.heappush(time, (start, end))
+# time.sort(key=lambda x : x[1]) #시작 시간으로 정렬하는 이유: 이전 강의가 끝나는 시간에 맞춰서 다른 강의를 할당해 줄때 시작 시간이 빠른 순으로 할당해 줘야 최소의 강의실을 사용할 수 있음
 q = []
-# start, end = heapq.heappop(time)
+start, end = heapq.heappop(time)
 heapq.heappush(q, (end, start))
-# while time:
-for i in range(1, n):
+while time:
     prev_end, prev_start = heapq.heappop(q)
-    # start, end = heapq.heappop(time)
-    end, start = time[i]
-
+    start, end = heapq.heappop(time)
     if prev_end <= start: #이전 수업에 사용하던 강의실 배정
         heapq.heappush(q, (end, start))
     else: #새로운 강의실 배정
