2a01612bc9915cec1d95c153d7735e7bbf9230b0
xxyeon <jykim9335@gmail.com>
2024-10-15 14:17:12 +0900

    fix: 강의실 배정 시간초과 해결, input -> sys로 입력 받기

--- week04/강의실배정/김지연.py
+++ week04/강의실배정/김지연.py
@@ -1,16 +1,23 @@
 import heapq
-n = int(input())
+import sys
+n = int(sys.stdin.readline().rstrip())
 
 time = []
+
 for _ in range(n):
-    start, end = map(int, input().split())
+    start, end = map(int, sys.stdin.readline().rstrip().split())#input()으로 입력받을 시 시간초과
     time.append((end, start))
+    # heapq.heappush(time, (start, end))
 time.sort(key=lambda x : x[1]) #시작 시간으로 정렬하는 이유: 이전 강의가 끝나는 시간에 맞춰서 다른 강의를 할당해 줄때 시작 시간이 빠른 순으로 할당해 줘야 최소의 강의실을 사용할 수 있음
 q = []
-heapq.heappush(q, time[0])
+# start, end = heapq.heappop(time)
+heapq.heappush(q, (end, start))
+# while time:
 for i in range(1, n):
     prev_end, prev_start = heapq.heappop(q)
+    # start, end = heapq.heappop(time)
     end, start = time[i]
+
     if prev_end <= start: #이전 수업에 사용하던 강의실 배정
         heapq.heappush(q, (end, start))
     else: #새로운 강의실 배정
