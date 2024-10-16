da20a96b5b45afa79b86c278ec1280d77af18fae
suji <csj1430@naver.com>
2024-10-16 15:31:34 +0900

    Style: 주석 삭제

--- week04/이친수/조수지.py
+++ week04/이친수/조수지.py
@@ -19,37 +19,3 @@ else :
     pinary[i] = pinary[i - 1] + pinary[i - 2]
   
   print(pinary[n])
-
-
-# 힙을 사용한 코드... 시간 초과
-'''import heapq
-import sys
-input = sys.stdin.readline
-
-n = int(input()) # n자리 이진수
-
-if n == 1 or n == 2 : # 1) 1, 2) 10
-  print(1)
-
-else :
-  pinary = [] # 이친수 저장할 배열
-  cnt = 0 # 이친수 개수
-  
-  heapq.heappush(pinary, '1') # 이친수는 1로 시작하므로 넣어줌
-  
-  while pinary :
-    now = heapq.heappop(pinary)
-    
-    if len(now) == n : #
-      cnt += 1
-    
-    else :
-      if now[-1] == '0' :
-        heapq.heappush(pinary, now + '0')
-        heapq.heappush(pinary, now + '1')
-      
-      # 1이 등장하면 그 다음은 '무조건' 0이 되어야 함    
-      elif now[-1] == '1' :
-        heapq.heappush(pinary, now + '0')
-  
-  print(cnt)'''
