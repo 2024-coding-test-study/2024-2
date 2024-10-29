35f71d072984f3c13478663072191a6ab8e17b30
xxyeon <jykim9335@gmail.com>
2024-10-30 00:54:36 +0900

    Add: 파일합치기3

---
+++ week05/파일합치기3/김지연.py
@@ -0,0 +1,19 @@
+#새로운 파일이 생기면서 기존에 있는 파일들의 비용과 새로 만들어진 파일 비용을 비교해야하므로 우선순위 큐 사용
+import heapq
+import sys
+t = int(sys.stdin.readline().rstrip()) #input()으로 입력받아서 시간초과는 나지 않음
+total_cost = []
+for _ in range(t):
+    cost = 0
+    q = []
+    k = int(sys.stdin.readline().rstrip())
+    files = list(map(int, sys.stdin.readline().rstrip().split()))
+    for i in range(k):
+        heapq.heappush(q, files[i])
+    while len(q) > 1:
+        cost1 = heapq.heappop(q)
+        cost2 = heapq.heappop(q)
+        cost += cost1+cost2 #새파일 만드는데 발생하는 비용
+        
+        heapq.heappush(q, cost1+cost2)
+    print(cost)
