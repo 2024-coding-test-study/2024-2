bce42e027af4b5b14c2cc71ec3b691ef1f6863f3
xxyeon <jykim9335@gmail.com>
2024-11-06 10:02:07 +0900

    fix: 16401

--- week06/16401/김지연.py
+++ week06/16401/김지연.py
@@ -1,39 +1,23 @@
-import heapq
 m, n = map(int, input().split())
 lst = list(map(int, input().split()))
 lst.sort() #과자 길이 정렬 후 이분 탐색 
-copy_lst = [0] * n
-for i in range(len(lst)):
-    copy_lst[i] = lst[i]
 
-#주어진 과자 길이 모두 더한 후 조카 수로 나눠서 최대 길이 구하기
-answer = sum(lst) // m
+#과자 길이를 대상으로 이분 탐색
+answer = 0
+start, end = 0, lst[-1] #start = 0으로 할 경우 mid = 0이 될 수도 있음 -> `cnt += lst[i] mid` 에서 zeroDivisionError
 
-#이분 탐색
-def binary_search(start, end, target):
-    while start <= end:
-        
-        mid = (start + end) // 2
-        
-        if lst[mid] >= target: #최대로 나눠줄 수 있는 막대 과자 길이(target) 보다 길거나 같으면 리턴
-            lst[mid] = lst[mid] - target
-            return mid
-        elif lst[mid] < target:
-            start = mid + 1
-    return False
-        
-while answer != 0: #과자를 나눠줄 수 없을 때까지 반복
-    #막대길이(answer)를 m명에게 나눠줄 수 있는지 확인
-    mid = 0
-    for i in range(m):
-        mid = binary_search(mid, n-1, answer) #m개의 막대길이 (answer)를 lst에서 이분 탐색
-        lst.sort()
-    if mid == False:
-        for i in range(len(lst)):
-            lst[i] = copy_lst[i]
-        answer -= 1
-    else:
+while start <= end: 
+    mid = (start + end) // 2
+    if start == end and start == 0 and mid == 0:
         break
+
+    cnt = 0
+    for i in range(n): #과자길이 mid에 대해서 몇명에게 나눠줄 수 있는지 count
+        cnt += lst[i] // mid
+    if cnt < m: #나눠줄수있는 과자가 조카 수보다 적다면 -> 과자 길이 줄이기
+        end = mid - 1
+    elif cnt >= m: #조카 수보다 많이 나눠줄 수 있다면 -> 과자길이 늘리기
+        answer = mid
+        start = mid + 1
+
 print(answer)
-
-
