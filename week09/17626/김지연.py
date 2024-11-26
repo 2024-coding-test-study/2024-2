a9475ff1f79f5e32e2049937ab4ed35d120aa2a8
xxyeon <jykim9335@gmail.com>
2024-11-27 05:39:05 +0900

    add: Four Squares 87%에서 틀림

--- week09/17626/김지연.py
+++ week09/17626/김지연.py
@@ -1,21 +1,38 @@
 import math
 n = int(input())
 num = int(math.sqrt(n))# n의 제곱근 보다 작은 정수 저장
-lst = [0] * (num + 1)
-result = 5
-for i in range(1, num+1):
-    lst[i] = i**2
-for i in range(num, 0, -1):
-    cnt = 0
-    now = lst[i]
-    start = n
-    while start > 0:
-        
-        start -= now #이 수 이후의 값이 들어갔을때
-        now = lst[int(math.sqrt(start))]
-        cnt += 1
-        
-    result = min(result, cnt)
-    
+lst = [i**2 for i in range(1, num+1)]
 
-print(result) 
+#1,2,3,4 최소 개수 종류
+find = False
+if n in lst:
+    find = True
+    print(1)
+else:
+    #최소 개수가 2개인 경우라면
+    for i in lst:
+        for j in lst:
+            if i + j == n:
+                find = True
+                print(2)
+                break
+            if find:
+                break
+        if find:
+            break
+                
+    #최소 개수가 3개라면
+    for i in lst:
+        for j in lst:
+            for k in lst:
+                if i + j + k == n:
+                    find = True
+                    print(3)
+                    break
+            if find:
+                break
+        if find:
+            break
+ 
+if not find:
+    print(4)
